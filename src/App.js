8d392a5820688dad511963156563f17ed006e89e
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-18 15:50:14 +0200

    adding initial reducers

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
 import './App.css';
-import { useState } from 'react';
-import axios from 'axios';
+import { useReducer, useState } from 'react';
 import QuestionsList from './QuestionsList';
-import { QuestionsContext } from './TriviaContext';
+import {QuestionsContext, questionsReducer } from './TriviaContext';
 import Results from './Results';
+import axios from 'axios';
+
 
 function App() {
 
-  const [questions, setQuestions] = useState([])
+  // const [questions, setQuestions] = useState([])
+  const [questions, dispatch] = useReducer(questionsReducer, [])
+
   const [gameInProgress, setGameInProgress] = useState(false)
 
-  const handleStartGame = async () => {
-    const response = await axios.get("https://the-trivia-api.com/api/questions?limit=5")
-    if (response.status === 200) {
 
-      // add selectedAnswer field to store user's answer
-      response.data.forEach(q => {
-        q.selectedAnswer = ""
-      });
-      setQuestions(response.data)
-      setGameInProgress(true)
-    }
+  const handleStartGame = async () => {
+      const response = await axios.get("https://the-trivia-api.com/api/questions?limit=5")
+      if (response.status === 200) {
+              // setQuestions(response.data)
+              dispatch({
+                  type: 'start',
+                  questionsReceived: response.data
+              })
+              setGameInProgress(true)
+      }
+
+
   }
 
   const handleSelectAnswer = (questionId, answer) => {
-    const selectedQuestion = questions.filter(q => q.id === questionId)[0]
-    selectedQuestion.selectedAnswer = answer
-    setQuestions([...questions])
+    dispatch({
+      type: 'selectAnswer',
+      questionId: questionId,
+      answer: answer
+    })
+  }
+
+  const handleResetAnswers = () => {
+    dispatch({
+      type: 'resetAnswers'
+    })
   }
 
   const handleSubmitGame = () => {
     setGameInProgress(false)
   }
 
-  const handleResetAnswers = () => {
-    questions.forEach(q => {
-      q.selectedAnswer = ''
-    })
-    setQuestions([...questions])
-  }
-
   return (
-    <div className='App'>
+      <div className='App'>
       <h3>TRIVIA GAME</h3>
 
-      <QuestionsContext.Provider value={{questions, handleSelectAnswer, handleSubmitGame, handleResetAnswers}}>
-        {gameInProgress &&
-          <QuestionsList />
-        }
-        {!gameInProgress && questions.length > 0 &&
-          <Results />
-        }
-      </QuestionsContext.Provider>
+      {questions.length > 0 &&
+        <QuestionsContext.Provider value={{questions, handleSelectAnswer, handleSubmitGame, handleResetAnswers}}>
+          
+          {gameInProgress ?
+          	<QuestionsList />
+          :
+          	<Results />
+		  }
+          
+        </QuestionsContext.Provider>
+      }
 
       {!gameInProgress &&
         <button onClick={handleStartGame}>NEW GAME</button>
--- src/TriviaContext.js
+++ src/TriviaContext.js
@@ -1,3 +1,34 @@
 import { createContext } from "react";
 
-export const QuestionsContext = createContext([])
+
+export const QuestionsContext = createContext(null)
+
+export function questionsReducer(questions, action) {
+    switch (action.type) {
+
+        case 'start': {
+            action.questionsReceived.forEach(q => {
+                q.selectedAnswer = ""
+            });
+            console.log('returning from start', action.questionsReceived)
+            return action.questionsReceived
+        }
+        case 'selectAnswer': {
+            const selectedQuestion = questions.filter(q => q.id === action.questionId)[0]
+            selectedQuestion.selectedAnswer = action.answer
+            return [...questions]
+        }
+
+        case 'resetAnswers': {
+            questions.forEach(q => {
+                q.selectedAnswer = ''
+              })
+            return [...questions]
+        }
+
+        default: {
+            throw Error('Unknown action: ' + action.type)
+        }
+
+    }
+}
